5b3db5b558f38c0d85a595d1519f9ea77b195fb5
youngtrashbag <yuule.smeker@gmail.com>
2020-02-20 20:50:03 +0100

    better folder structure

---
+++ src/increment/func.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: youngtrashbag
+ * Date: 20.02.2020
+ * File: increment.cpp
+ */
+
+#include "increment.h"
+
+using namespace std;
+
+void incrementVector(vector<unsigned char> &imgVector)
+{
+	//do stuff
+	//1. skip header
+	//2. increment other chars
+}
+
