6d873ab62dab665a411c64dbd9fb9b69eb1c3804
youngtrashbag <yuule.smeker@gmail.com>
2020-03-19 15:11:46 +0100

    actually fix the crc error that was b4

--- src/chunk/crc.cpp
+++ src/chunk/crc.cpp
@@ -15,7 +15,7 @@ using namespace std;
 unsigned long Chunk::LoadCRC()
 {
 	// add the remaining 3 chars of the type and the length of the chunk, to see the crc
-	vector<unsigned char>::iterator crcFinder = _typePos + 2 + _length;
+	vector<unsigned char>::iterator crcFinder = _typePos + 3 + _length;
 
 	// get size of chunk
 	unsigned char charCRCArray[5] = {
@@ -29,10 +29,8 @@ unsigned long Chunk::LoadCRC()
 
 	for(int i = 0; i < 4; i++)
 	{
-		cyclicRedundancyCheck += charCRCArray[i];
-		cout << "b4 bitshift: " << cyclicRedundancyCheck << endl;
 		cyclicRedundancyCheck = cyclicRedundancyCheck << 8;
-		cout << "after bitshift: " << cyclicRedundancyCheck << endl;
+		cyclicRedundancyCheck += charCRCArray[i];
 	}
 
 	return cyclicRedundancyCheck;
