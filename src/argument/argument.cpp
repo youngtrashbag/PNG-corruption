d141611fa599207b42432dd02ed833fc84221923
youngtrashbag <yuule.smeker@gmail.com>
2020-03-26 10:13:43 +0100

    fill out basic Skeleton of ProcessArgs Function

--- src/argument/argument.cpp
+++ src/argument/argument.cpp
@@ -12,9 +12,57 @@ using namespace std;
 
 void ProcessArgs(int argc, char* argv[])
 {
-	for(unsigned char argcount = 1; argcount < argc; argcount++)
+	bool help = false;
+	string output = "";
+	int increment = 0, shift = 0;
+
+	for(int argcount = 1; argcount < argc; argcount++)
 	{
 		cout << argv[argcount] << endl;
+
+		if(argv[argcount] == "-h"
+			|| argv[argcount] == "--help")
+		{
+			help = true;
+		}
+
+		if(argv[argcount] == "-o"
+			|| argv[argcount] == "--output")
+		{
+			if(argcount + 1 <= argc)
+				output = argv[argcount + 1];
+		}
+
+		if (argv[argcount] == "--increment")
+		{
+			if(argcount + 1 <= argc)
+				increment = argv[argcount + 1];
+		}
+
+		if (argv[argcount] == "--shift")
+		{
+			if(argcount + 1 <= argc)
+				shift = argv[argcount + 1];
+		}
+	}
+
+	if(help)
+	{
+		// TODO: display help dialog
+	}
+	else if(output != "")
+	{
+		// output filename is not empty
+	}
+
+	// increment is preferred to shift
+	if(increment != 0)
+	{
+		// return increment
+	}
+	else if(shift != 0)
+	{
+		// return shift
 	}
 }
 
